fd8e860ca30f18866bae09e097d5e20004eff132
wangding <1337312919@qq.com>
2018-10-08 10:06:37 +0800

    提交

---
+++ 作业1/任务二.js
@@ -0,0 +1,40 @@
+function getStyle(obj,attr){
+			if(obj.currentStyle){
+				return obj.currentStyle[attr];
+			}
+			else{
+				return getComputedStyle(obj,null)[attr];
+			}
+		}
+		function animate(obj,json,callback){
+			clearInterval(obj.timer);
+			obj.timer=setInterval(function(){
+				var isStop = true;
+				for(var attr in json){//遍历对象
+					var now=0;
+					if(attr=='opacity'){
+						now=parseInt(getStyle(obj,attr)*100);
+					}
+					else{
+						now=parseInt(getStyle(obj,attr));
+					}
+					var speed=(json[attr]-now)/5;
+					speed = speed >0?Math.ceil(speed):Math.floor(speed);
+					var current=now + speed;
+					if(attr=='opacity'){
+						obj.style[attr]=current/100;
+					}
+					else{
+						obj.style[attr]=current + "px";
+					}
+					
+					if(current !==json[attr]){
+						isStop=false;
+					}
+				}
+				if(isStop){
+					clearInterval(obj.timer);
+					callback&&callback();
+				}
+			},30)
+		}
